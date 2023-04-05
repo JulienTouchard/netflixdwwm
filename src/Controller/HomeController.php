d5c1567f3c11c577a2dd705f44f7cf713bc4f68f
JulienTouchard <julien.touchard@free.fr>
2023-04-05 13:11:21 +0200

    projet netflix suite

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,11 +12,45 @@ class HomeController extends AbstractController
     #[Route('/ ', name: 'app_home')]
     public function index(MoviesFullRepository $moviesFullRepository): Response
     {
-        $films2011 = $moviesFullRepository->findBy(["year"=>2011]);
-        //dd($films2011);
+        $posterDirectory = str_replace("\\","/",$this->getParameter('assets'))."/img/posters/";
+        
+        function randFilms($genre, $nb, $moviesFullRepository,$posterDirectory)
+        {
+            $films = $moviesFullRepository->findByGenres($genre);
+            $min = 0;
+            $max = count($films);
+            $arrayFilm = [];
+            $arrayReturn = [];
+            $i = 0;
+            while ($i < $nb) {
+                if (!in_array(rand($min, $max), $arrayFilm)) {
+                    array_push($arrayFilm, rand($min, $max));
+
+                    array_push($arrayReturn, $films[rand($min, $max)]);
+                    //dd($arrayReturn[count($arrayReturn)-1]->id);
+                    $id = $films[rand($min, $max)]->getId();
+                   
+                    if(file_exists($posterDirectory.$id.".jpg")){
+                        //$arrayReturn[$i]
+                    } else {
+                        
+                    }
+                } else {
+                    $i--;
+                }
+                $i++;
+            }
+            return $arrayReturn;
+        }
+        
+        randFilms('action', 10, $moviesFullRepository,$posterDirectory);
+        
+        
+        //$films2011 = $moviesFullRepository->findBy(["year"=>2011]);
+        //dd($filmsByGenre);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'films2011' => $films2011,
+            'filmsGenresAction' => randFilms('action', 10, $moviesFullRepository,$posterDirectory),
         ]);
     }
 }
--- src/Repository/MoviesFullRepository.php
+++ src/Repository/MoviesFullRepository.php
@@ -39,40 +39,49 @@ class MoviesFullRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return MoviesFull[] Returns an array of MoviesFull objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return MoviesFull[] Returns an array of MoviesFull objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('m.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?MoviesFull
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
-
-    public function findRand($nb): ?MoviesFull
+    //    public function findOneBySomeField($value): ?MoviesFull
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+    /**
+     * Ma jolie méthode pour sélectionner des films par genre
+     */
+    public function findByGenres($genre)
     {
         return $this->createQueryBuilder('m')
-        ->orderBy('RAND()')
-        ->setMaxResults($nb)
-        ->getQuery()
-        ->getResult()
-        ;
+            ->andWhere('m.genres LIKE :genre')
+            ->setParameter('genre', "%$genre%")
+            ->getQuery()
+            ->getResult();
+    }
 
-
+    public function findRand($nb)
+    {
+        return $this->createQueryBuilder('m')
+            ->addSelect('RAND() as HIDDEN rand')
+            ->orderBy('rand')
+            ->setMaxResults($nb)
+            ->getQuery()
+            ->getResult();
     }
 }
